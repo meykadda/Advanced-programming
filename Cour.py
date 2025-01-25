78464c11ef5b7c0c94a7d7b705c1b63e3b31d350
project00-dot <kaddachemey@gmail.com>
2025-01-25 16:38:07 +0100

    .

---
+++ Cour.py
@@ -0,0 +1,12 @@
+#while True:
+#    print("Hi")
+#    answer=str(input("shall we continue?")).capitalize()
+#    if answer=="no":
+#        print("ok then")
+#        break
+#    print("okey then")
+
+################################################################"
+
+num=int(input("enter an integer number"))
+if num<
